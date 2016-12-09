45aa5e4cdb342b76dac3e7ad85be7c330cdf71f9
Einar W. Høst <einarwh@gmail.com>
2016-12-09 14:09:16 +0100

    Little less broken

--- Coup/Coup/Program.fs
+++ Coup/Coup/Program.fs
@@ -1,7 +1,7 @@
 ﻿open Mono.Cecil
 open Mono.Cecil.Cil
 
-type EdgeKind = Calls | Implements | Inherits | Offers | Accepts | Returns | Holds
+type EdgeKind = Calls | Implements | Inherits | Offers | Accepts | Returns | Holds | Annotates | Exposes
 
 type DependencyEdge = 
   { kind : EdgeKind 
@@ -19,43 +19,17 @@ type TypeDependencies =
   { source : TypeDefinition
     dependencies : TargetDependency list }
 
-//type Dependency = 
-//  { source : TypeDefinition
-//    kind : EdgeKind
-//    target : TypeReference }
-
-type Edge<'T> = 
-  { kind : EdgeKind 
-    tgt : 'T }
-and 
-  TypeNode = 
-  { name : string
-    ``namespace`` : string 
-    typeDef : TypeDefinition
-    edges : Edge<TypeNode> list }
-
-let typeReferredByInstruction (inst : Instruction) : Edge<string> option = 
-   match inst.OpCode.Code with
-   | Code.Call -> 
-     match inst.Operand with
-     | :? GenericInstanceMethod as gim -> 
-       printfn "generic instance method"
-       //gim.GenericArguments |> printfn 
-     | :? MethodDefinition as mdef ->
-       printfn "method definition"
-     | :? MethodReference as mref ->
-       printfn "method reference"
-     | _ ->
-       printfn "something entirely different or similar?"
-   | _ ->
-     ()
-   None
-
 let rec typesFromTypeReference (typeRef : TypeReference) : TypeReference list = 
   match typeRef with
      | :? GenericInstanceType as genericInstanceType -> 
        // printfn "generic instance type with element type %s" genericInstanceType.ElementType.FullName
-       [ typeRef ]
+       let gen = 
+         if genericInstanceType.HasGenericArguments then
+           genericInstanceType.GenericArguments |> Seq.filter (fun arg -> not arg.IsGenericParameter) |> Seq.collect (fun arg -> arg |> typesFromTypeReference) |> Seq.toList
+         else
+           []
+       let result = [ typeRef ] @ gen
+       result
      | :? ArrayType as arrayType ->
        // Unwrap array
        typesFromTypeReference arrayType.ElementType
@@ -64,6 +38,30 @@ let rec typesFromTypeReference (typeRef : TypeReference) : TypeReference list =
        [ typeRef ]
      | _ ->
        [ typeRef ]
+
+let typesReferredByCallInstruction (inst : Instruction) : DependencyEdge list = 
+  let maybeDeclaringType : TypeReference option = 
+    match inst.Operand with
+    | :? GenericInstanceMethod as gim -> 
+      Some gim.DeclaringType
+    | :? MethodDefinition as mdef ->
+      let tref = mdef.DeclaringType :> TypeReference
+      Some tref
+    | :? MethodReference as mref ->
+      Some mref.DeclaringType 
+    | _ ->
+      printfn "something entirely different or similar?"
+      None
+  match maybeDeclaringType with
+  | Some declaringType -> declaringType |> typesFromTypeReference |> List.map (fun tgt -> { kind = Calls; target = tgt })
+  | None -> []
+
+let typeReferredByInstruction (inst : Instruction) : DependencyEdge list = 
+   match inst.OpCode.Code with
+   | Code.Call -> 
+     typesReferredByCallInstruction inst
+   | _ ->
+     []
 
 let typesReferredByField (fieldDef : FieldDefinition) : DependencyEdge list = 
   let fieldType = fieldDef.FieldType
@@ -80,14 +78,26 @@ let typesReferredByMethodReturnType (methodDef : MethodDefinition) : DependencyE
   methodDef.ReturnType |> typesFromTypeReference |> List.map (fun tr -> { kind = Returns; target = tr })
 
 let typesReferredByMethodBody (methodDef : MethodDefinition) : DependencyEdge list = 
-  let flops = methodDef.Body.Instructions |> Seq.map (fun inst -> typeReferredByInstruction inst) |> Seq.toList
-  []
+  let result = methodDef.Body.Instructions |> Seq.collect (fun inst -> typeReferredByInstruction inst) |> Seq.toList
+  result
 
 let typesReferredByMethod (methodDef : MethodDefinition) : DependencyEdge list = 
   let fromParameters = typesReferredByMethodParameters methodDef
   let fromReturnType = typesReferredByMethodReturnType methodDef
   let fromMethodBody = if methodDef.HasBody then typesReferredByMethodBody methodDef else []
   let result = fromParameters @ fromReturnType @ fromMethodBody
+  result
+
+let typesReferredByProperty (propDef : PropertyDefinition) : DependencyEdge list = 
+  let fromPropertyAccessor (methodDef : MethodDefinition) = 
+    match methodDef with
+    | null -> []
+    | md when md.HasBody -> typesReferredByMethodBody methodDef
+    | md -> []
+  let fromGet = propDef.GetMethod |> fromPropertyAccessor
+  let fromSet = propDef.SetMethod |> fromPropertyAccessor
+  let fromPropType = propDef.PropertyType |> typesFromTypeReference |> List.map (fun tr -> { kind = Exposes; target = tr })
+  let result = fromPropType @ fromGet @ fromSet
   result
 
 let collapseEdges (edges : DependencyEdge list) : WeightedKind list = 
@@ -103,16 +113,25 @@ let typesReferredByClass (classDef : TypeDefinition) : TypeDependencies =
     | null -> []
     | t -> typesFromTypeReference t |> List.map (fun tgt -> { kind = EdgeKind.Inherits; target = tgt })
   let fromMethods = classDef.Methods |> Seq.collect (fun methodDef -> typesReferredByMethod methodDef) |> Seq.toList
+  let fromProperties = classDef.Properties |> Seq.collect (fun propDef -> typesReferredByProperty propDef) |> Seq.toList
   let fromFields = classDef.Fields |> Seq.collect (fun fieldDef -> typesReferredByField fieldDef) |> Seq.toList
-  let edges = fromInterfaces @ fromBaseClass @ fromMethods @ fromFields
+  let edges = fromInterfaces @ fromBaseClass @ fromMethods @ fromProperties @ fromFields
   let deps = edges |> List.groupBy (fun edge -> edge.target.FullName) |> List.map (fun (tgt, edges) -> { target = (edges |> List.head |> (fun it -> it.target)); kinds = collapseEdges edges })
   { source = classDef
     dependencies = deps }
 
-let typesReferredByInterfaceDefinition (interfaceDef : TypeDefinition) : Edge<string> list = 
+let typesReferredByInterface (interfaceDef : TypeDefinition) : TypeDependencies =
+  let fromInterfaces : DependencyEdge list = 
+    interfaceDef.Interfaces 
+    |> Seq.collect (fun interfaceRef -> [ { kind = EdgeKind.Implements; target = interfaceRef } ] ) 
+    |> Seq.toList
   let fromMethods = interfaceDef.Methods |> Seq.collect (fun methodDef -> typesReferredByMethod methodDef) |> Seq.toList
+  let fromProperties = interfaceDef.Properties |> Seq.collect (fun propDef -> typesReferredByProperty propDef) |> Seq.toList
   let fromFields = interfaceDef.Fields |> Seq.collect (fun fieldDef -> typesReferredByField fieldDef) |> Seq.toList
-  []
+  let edges = fromInterfaces @ fromMethods @ fromProperties @ fromFields
+  let deps = edges |> List.groupBy (fun edge -> edge.target.FullName) |> List.map (fun (tgt, edges) -> { target = (edges |> List.head |> (fun it -> it.target)); kinds = collapseEdges edges })
+  { source = interfaceDef
+    dependencies = deps }
 
 let (|ClassDefinition|_|) (typeDef : TypeDefinition) =
    if typeDef.IsClass then Some (ClassDefinition typeDef) else None
@@ -123,12 +142,13 @@ let (|InterfaceDefinition|_|) (typeDef : TypeDefinition) =
 let (|EnumDefinition|_|) (typeDef : TypeDefinition) =
    if typeDef.IsEnum then Some (EnumDefinition typeDef) else None
 
-let getTypeDependencies (typeDef : TypeDefinition) : Edge<string> list = 
-   match typeDef with
-   | ClassDefinition cd -> []
-   | InterfaceDefinition id -> typesReferredByInterfaceDefinition id
-   | EnumDefinition ed -> []
-   | _ -> []
+let getLinesForType (typeDeps : TypeDependencies) = 
+  match typeDeps with
+  | { source = s; dependencies = deps } ->
+    let edges = deps |> List.map (fun it -> sprintf "%s -> %s;" s.FullName it.target.FullName)
+    let shape = if s.IsInterface then "ellipse" else "box"
+    let node = sprintf "%s [label = \"%s\" shape = \"%s\"];" s.FullName s.Name shape
+    node :: edges
 
 [<EntryPoint>]
 let main argv = 
@@ -139,43 +159,53 @@ let main argv =
     //types |> Seq.iter (fun typeDef -> printfn "%A" typeDef.Name; typeDef.CustomAttributes |> Seq.iter (fun attr -> printfn "  -> %A" attr.AttributeType.Name))
     let allClasses = types |> Seq.filter (fun typeDef -> typeDef.IsClass) |> Seq.toList
     let genClasses = allClasses |> Seq.filter (fun typeDef -> typeDef.HasCustomAttributes && typeDef.CustomAttributes |> Seq.exists (fun a -> a.AttributeType.Name = "CompilerGeneratedAttribute"))
-    let classes = allClasses |> Seq.except genClasses |> Seq.toList
-    let interfaces = types |> Seq.filter (fun typeDef -> typeDef.IsInterface) |> Seq.toList
     let enums = types |> Seq.filter (fun typeDef -> typeDef.IsEnum) |> Seq.toList
+    let interfaces = types |> Seq.filter (fun typeDef -> typeDef.IsInterface) |> Seq.toList
+    let classes = allClasses |> Seq.except genClasses |> Seq.except enums |> Seq.except interfaces |> Seq.toList
 
-    printfn "Classes"
-    printfn "-------"
-    classes |> Seq.iter (fun it -> printfn "%s" it.Name)
-    printfn ""
+    let typeList = classes @ interfaces @ enums
+    let typeNames = typeList |> List.map (fun t -> t.FullName)
 
-    let foo = classes |> Seq.map (fun cd -> typesReferredByClass cd) |> Seq.toList
+//    printfn "Classes"
+//    printfn "-------"
+//    classes |> Seq.iter (fun it -> printfn "%s" it.Name)
+//    printfn ""
+//
+//    printfn "Interfaces"
+//    printfn "----------"
+//    interfaces |> Seq.iter (fun it -> printfn "%s" it.Name)
+//    printfn ""
+//
+//    printfn "Enums"
+//    printfn "-----"
+//    enums |> Seq.iter (fun it -> printfn "%s" it.Name)
+//    printfn ""
+
+    let classTypeDependencies = classes |> Seq.map (fun cd -> typesReferredByClass cd) |> Seq.toList
+
+    let interfaceTypeDependencies = interfaces |> Seq.map (fun def -> typesReferredByInterface def) |> Seq.toList
 
     let formatWeightedKind wk = 
       match wk with
       | {kind = k; weight = w} -> (k, w)
 
-    foo |> List.iter (fun it -> printfn "type definition: %A" it.source; it.dependencies |> List.iter (fun d -> printfn " -> %s %A" d.target.FullName (d.kinds |> List.map (fun k -> formatWeightedKind k))))
+    let filteredClassTypeDependencies = 
+      classTypeDependencies 
+      |> List.map (fun { source = s; dependencies = deps } -> { source = s; dependencies = deps |> List.filter (fun d -> typeNames |> List.contains d.target.FullName) |> List.filter (fun d -> not (d.target.FullName.Equals(s.FullName)) ) })
 
-    printfn "lol %d" foo.Length
+    //filteredClassTypeDependencies |> List.iter (fun it -> printfn "class type definition: %A" it.source; it.dependencies |> List.iter (fun d -> printfn " -> %s %A" d.target.FullName (d.kinds |> List.map (fun k -> formatWeightedKind k))))
 
-    printfn "Interfaces"
-    printfn "----------"
-    interfaces |> Seq.iter (fun it -> printfn "%s" it.Name)
-    printfn ""
+    let filteredInterfaceTypeDependencies = 
+      interfaceTypeDependencies 
+      |> List.map (fun { source = s; dependencies = deps } -> { source = s; dependencies = deps |> List.filter (fun d -> typeNames |> List.contains d.target.FullName) |> List.filter (fun d -> not (d.target.FullName.Equals(s.FullName)) ) })
 
-    printfn "Enums"
-    printfn "-----"
-    enums |> Seq.iter (fun it -> printfn "%s" it.Name)
-    printfn ""
+    let allTypeDeps = filteredClassTypeDependencies @ filteredInterfaceTypeDependencies
+    let lines = allTypeDeps |> List.collect (fun typeDef -> typeDef |> getLinesForType)
+    let indentedLines = lines |> List.map (fun line -> sprintf "  %s" line)
+    let oneBigLine = indentedLines |> String.concat "\n"
+    let graph = sprintf "digraph g {\n%s}" oneBigLine
+    printfn "%s" graph
 
-    let addTypeDependencyEdges typeNode typeNodes = 
-      typeNode
-
-    let typeNodes = types |> Seq.map (fun typeDef -> { name = typeDef.Name 
-                                                       ``namespace`` = typeDef.Namespace
-                                                       typeDef = typeDef
-                                                       edges = List.empty })
-
-    let typeNodes' = typeNodes |> Seq.map (fun typeNode -> addTypeDependencyEdges typeNode typeNodes)
+    //filteredInterfaceTypeDependencies |> List.iter (fun it -> printfn "interface type definition: %A" it.source; it.dependencies |> List.iter (fun d -> printfn " -> %s %A" d.target.FullName (d.kinds |> List.map (fun k -> formatWeightedKind k))))
 
     0 // return an integer exit code
